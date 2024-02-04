8fc1c48820bba538c4d9b151751b61af0d29e010
DESKTOP-PM3TPIM\admin <ejrdud1256@gmail.com>
2024-02-04 15:31:17 +0900

    JoinCode 인풋필드 연결 추가

--- Assets/Script/UiManager.cs
+++ Assets/Script/UiManager.cs
@@ -10,24 +10,26 @@ public class UiManager : MonoBehaviour
 {
     public Button Host;
     public Button Client;
-    public TMP_InputField InputField;
+    public TMP_InputField inputField;
 
     private void Start()
     {
         Host.onClick.AddListener(async () =>
         {
-            var data = await RelayManager.SetUpRelay(10, "UnityNetcodeTest");
+            var data = await RelayManager.SetUpRelay(10, "NetCodeTest");
 
             NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(data.IPv4Address, data.Port, data.AllocationIDByte, data.Key, data.ConnectionData);
+
+            inputField.text = data.JoinCode;
 
             NetworkManager.Singleton.StartHost();
         });
 
         Client.onClick.AddListener(async () =>
         {
-            var data = await RelayManager.JoinRelay(InputField.text, "UnityNetcodeTest");
+            var data = await RelayManager.JoinRelay(inputField.text, "NetCodeTest");
 
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(data.IPv4Address, data.Port, data.AllocationIDByte, data.Key, data.ConnectionData);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(data.IPv4Address, data.Port, data.AllocationIDByte, data.Key, data.ConnectionData, data.HostConnectionData);
 
             NetworkManager.Singleton.StartClient();
         });
